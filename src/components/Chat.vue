eb7b44168fc9e952174ccdc3ab2ff4821b9f1edd
Karim Aouadi <kdolphinus@gmail.com>
2018-07-25 23:47:09 +0200

    Added event listener

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -4,10 +4,10 @@
     <div class="card">
         <div class="card-content">
             <ul class="messages">
-                <li>
-                    <span class="teal-text">Name</span>
-                    <span class="grey-text text-darken-3">message</span>
-                    <span class="grey-text time">time</span>
+                <li v-for="message in messages" :key="message.id" >
+                    <span class="teal-text">{{message.name}} </span>
+                    <span class="grey-text text-darken-3">{{message.content}}</span>
+                    <span class="grey-text time">{{message.timestamp}}</span>
 
                 </li>
             </ul>
@@ -24,6 +24,8 @@
 
 <script>
 import NewMessage from "@/components/NewMessage";
+import db from "@/firebase/init";
+import moment from "moment";
 export default {
     name: 'Chat',
     props: ['name'],
@@ -33,7 +35,28 @@ export default {
     data(){
         return {
 
+            messages: []
+
         }
+    },
+    created() {
+      let ref = db.collection('messages').orderBy('timestamp')
+      ref.onSnapshot(snapshot => {
+        console.log(snapshot.docChanges())
+        snapshot.docChanges().forEach(change => {
+            if(change.type == 'added'){
+                let doc = change.doc
+                this.messages.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    content: doc.data().content,
+                    timestamp: moment(doc.data().timestamp).format('lll')
+                })
+
+            }
+            
+        })
+      })
     }
 
 }
@@ -49,7 +72,7 @@ export default {
 }
 .chat .time{
     display: block;
-    font-size: 1.2em;
+    font-size: 0.8em;
 }
 
 </style>
